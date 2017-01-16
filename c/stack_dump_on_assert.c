135cbb6ab9026aba5f811cdf3a1c390a1dd14d69
Paul Foster <paul.foster@soartech.com>
2017-01-16 12:11:33 -0500

    better symbol lookup and c++ compat

--- c/stack_dump_on_assert.c
+++ c/stack_dump_on_assert.c
@@ -3,15 +3,28 @@
 
 // Adapted by Paul Foster from rafael on https://oroboro.com/stack-trace-on-crash/, who has graciously put it in the public domain
 
-#include <execinfo.h>
-#include <errno.h>
-#include <cxxabi.h>
+//NOTE: I have tried to make this c compatible, but it won't demangle if you compile as c
+
+
+#include <stdio.h> //fprintf
+#include <signal.h>
+#include <execinfo.h> //basic stack info
+
+#include "stack_dump_on_assert.h"
 
 #ifdef __cplusplus
-extern "C" {
+    #include <errno.h>  //c++ demangle
+    #include <cxxabi.h> //c++ demangle
+    #include <cstdlib>
+
+    extern "C" {
+    static char** backtraceSymbols( void* const* addrList, int numAddr );
+#else
+    #include <stdlib.h>
 #endif
 
-static inline void printStackTrace( FILE *out = stderr, unsigned int max_frames = 63 );
+static inline void printStackTrace( FILE *out, unsigned int max_frames);
+static void abortHandler( int signum );
 
 
 void setup_stack_dump_on_assert()
@@ -46,34 +59,49 @@ static void abortHandler( int signum )
       fprintf( stderr, "Caught signal %d\n", signum );
  
    // Dump a stack trace.
-   printStackTrace();
+   printStackTrace(stderr,63);
  
    // If you caught one of the above signals, it is likely you just 
    // want to quit your program right now.
    exit( signum );
 }
 
-static inline void printStackTrace( FILE *out = stderr, unsigned int max_frames = 63 )
+static inline void printStackTrace( FILE *out, unsigned int max_frames)
 {
-   fprintf(out, "stack trace:\n");
- 
-   // storage array for stack trace address data
-   void* addrlist[max_frames+1];
- 
-   // retrieve current stack addresses
-   unsigned int addrlen = backtrace( addrlist, sizeof( addrlist ) / sizeof( void* ));
- 
-   if ( addrlen == 0 ) 
-   {
-      fprintf( out, "  \n" );
-      return;
-   }
- 
-   // resolve addresses into strings containing "filename(function+address)",
-   // Actually it will be ## program address function + offset
-   // this array must be free()-ed
-   char** symbollist = backtrace_symbols( addrlist, addrlen );
- 
+    fprintf(out, "stack trace:\n");
+
+    // storage array for stack trace address data
+    void* addrlist[max_frames+1];
+
+    // retrieve current stack addresses
+    unsigned int addrlen = backtrace( addrlist, sizeof( addrlist ) / sizeof( void* ));
+
+    if ( addrlen == 0 )
+    {
+        fprintf( out, "  \n" );
+        return;
+    }
+
+
+#ifndef __cplusplus
+    //IF USING PURE C, USE THIS AS THE REMAINDER OF THE FUNCTION
+    // resolve addresses into strings containing "filename(function+address)",
+    // Actually it will be ## program address function + offset
+    // this array must be free()-ed
+    char** symbollist = backtrace_symbols( addrlist, addrlen );
+    //IF USING PURE C, USE THIS AS THE REMAINDER OF THE FUNCTION
+    // print the stack trace.
+    unsigned int i;
+    for (i = 4; i < addrlen; i++ )
+        fprintf( out, "%s\n", symbollist[i]);
+
+    free(symbollist);
+#else //__cplusplus
+    // resolve addresses into strings containing "filename(function+address)",
+    // Actually it will be ## program address function + offset
+    // this array must be free()-ed
+    char** symbollist = backtrace_symbols( addrlist, addrlen );
+
    size_t funcnamesize = 1024;
    char funcname[1024];
  
@@ -132,7 +160,7 @@ static inline void printStackTrace( FILE *out = stderr, unsigned int max_frames 
             end_offset = p;
       }
  
-      if ( begin_name && end_offset && ( begin_name &lt; end_offset ))
+      if ( begin_name && end_offset && ( begin_name < end_offset ))
       {
          *begin_name++   = '\0';
          *end_offset++   = '\0';
@@ -164,9 +192,255 @@ static inline void printStackTrace( FILE *out = stderr, unsigned int max_frames 
          fprintf(out, "  %-40s\n", symbollist[i]);
       }
    }
- 
    free(symbollist);
+#endif //__cplusplus
 }
+/*//Stuff for using libbfd to look up symbols
+#include <bfd.h>    //c++ lookup file
+#include <dlfcn.h>  //c++ lookup file
+#include <link.h>   //c++ lookup file
+
+#include <string.h>
+#include <cassert>
+class FileLineDesc
+{
+public:
+   FileLineDesc( asymbol** syms, bfd_vma pc ) : mPc( pc ), mFound( false ), mSyms( syms ) {}
+
+   void findAddressInSection( bfd* abfd, asection* section );
+
+   bfd_vma      mPc;
+   char*        mFilename;
+   char*        mFunctionname;
+   unsigned int mLine;
+   int          mFound;
+   asymbol**    mSyms;
+};
+
+void FileLineDesc::findAddressInSection( bfd* abfd, asection* section )
+{
+   if ( mFound )
+      return;
+
+   if (( bfd_get_section_flags( abfd, section ) & SEC_ALLOC ) == 0 )
+      return;
+
+   bfd_vma vma = bfd_get_section_vma( abfd, section );
+   if ( mPc < vma )
+      return;
+
+   bfd_size_type size = bfd_section_size( abfd, section );
+   if ( mPc >= ( vma + size ))
+      return;
+
+   mFound = bfd_find_nearest_line( abfd, section, mSyms, ( mPc - vma ),
+                                   (const char**)&mFilename, (const char**)&mFunctionname, &mLine );
+}
+
+static void findAddressInSection( bfd* abfd, asection* section, void* data )
+{
+   FileLineDesc* desc = (FileLineDesc*)data;
+   assert( desc );
+   return desc->findAddressInSection( abfd, section );
+}
+
+static char** translateAddressesBuf( bfd* abfd, bfd_vma* addr, int numAddr, asymbol** syms )
+{
+   char** ret_buf = NULL;
+   int    total   = 0;
+
+   char   b;
+   char*  buf     = &b;
+   int    len     = 0;
+
+   for ( unsigned int state = 0; state < 2; state++ )
+   {
+      if ( state == 1 )
+      {
+         ret_buf = (char**)malloc( total + ( sizeof(char*) * numAddr ));
+         buf = (char*)(ret_buf + numAddr);
+         len = total;
+      }
+
+      for ( int i = 0; i < numAddr; i++ )
+      {
+         FileLineDesc desc( syms, addr[i] );
+
+         if ( state == 1 )
+            ret_buf[i] = buf;
+
+         bfd_map_over_sections( abfd, findAddressInSection, (void*)&desc );
+
+         if ( !desc.mFound )
+         {
+            total += snprintf( buf, len, "[0x%llx] \?\? \?\?:0", (long long unsigned int) addr[i] ) + 1;
+
+         } else {
+
+            const char* name = desc.mFunctionname;
+            if ( name == NULL || *name == '\0' )
+               name = "??";
+            if ( desc.mFilename != NULL )
+            {
+               char* h = strrchr( desc.mFilename, '/' );
+               if ( h != NULL )
+                  desc.mFilename = h + 1;
+            }
+            total += snprintf( buf, len, "%s:%u %s", desc.mFilename ? desc.mFilename : "??", desc.mLine, name ) + 1;
+            // elog << "\"" << buf << "\"\n";
+         }
+      }
+
+      if ( state == 1 )
+      {
+         buf = buf + total + 1;
+      }
+   }
+
+   return ret_buf;
+}
+
+static asymbol** kstSlurpSymtab( bfd* abfd, const char* fileName )
+{
+  if ( !( bfd_get_file_flags( abfd ) & HAS_SYMS ))
+  {
+     printf( "Error bfd file \"%s\" flagged as having no symbols.\n", fileName );
+     return NULL;
+  }
+
+  asymbol** syms;
+  unsigned int size;
+
+  long symcount = bfd_read_minisymbols( abfd, false, (void**)&syms, &size );
+  if ( symcount == 0 )
+       symcount = bfd_read_minisymbols( abfd, true,  (void**)&syms, &size );
+
+  if ( symcount < 0 )
+  {
+     printf( "Error bfd file \"%s\", found no symbols.\n", fileName );
+     return NULL;
+  }
+
+  return syms;
+}
+
+static char** processFile( const char* fileName, bfd_vma* addr, int naddr )
+{
+  bfd* abfd = bfd_openr( fileName, NULL );
+  if ( !abfd )
+  {
+     printf( "Error opening bfd file \"%s\"\n", fileName );
+     return NULL;
+  }
+
+  if ( bfd_check_format( abfd, bfd_archive ) )
+  {
+     printf( "Cannot get addresses from archive \"%s\"\n", fileName );
+     bfd_close( abfd );
+     return NULL;
+  }
+
+  char** matching;
+  if ( !bfd_check_format_matches( abfd, bfd_object, &matching ))
+  {
+     printf( "Format does not match for archive \"%s\"\n", fileName );
+     bfd_close( abfd );
+     return NULL;
+  }
+
+  asymbol** syms = kstSlurpSymtab( abfd, fileName );
+  if ( !syms )
+  {
+     printf( "Failed to read symbol table for archive \"%s\"\n", fileName );
+     bfd_close( abfd );
+     return NULL;
+  }
+
+  char** retBuf = translateAddressesBuf( abfd, addr, naddr, syms );
+
+  free( syms );
+
+  bfd_close( abfd );
+  return retBuf;
+}
+
+class FileMatch
+{
+public:
+  FileMatch( void* addr ) : mAddress( addr ), mFile( NULL ), mBase( NULL ) {}
+
+  void*       mAddress;
+  const char* mFile;
+  void*       mBase;
+};
+
+static int findMatchingFile( struct dl_phdr_info* info, size_t size, void* data )
+{
+  FileMatch* match = (FileMatch*)data;
+
+  for ( unsigned int i = 0; i < info->dlpi_phnum; i++ )
+  {
+     const ElfW(Phdr)& phdr = info->dlpi_phdr[i];
+
+     if ( phdr.p_type == PT_LOAD )
+     {
+        ElfW(Addr) vaddr = phdr.p_vaddr + info->dlpi_addr;
+        ElfW(Addr) maddr = ElfW(Addr)(match->mAddress);
+        if (( maddr >= vaddr ) &&
+            ( maddr < vaddr + phdr.p_memsz ))
+        {
+           match->mFile =        info->dlpi_name;
+           match->mBase = (void*)info->dlpi_addr;
+           return 1;
+        }
+     }
+  }
+  return 0;
+}
+
+static char** backtraceSymbols( void* const* addrList, int numAddr )
+{
+  char*** locations = (char***) alloca( sizeof( char** ) * numAddr );
+
+  // initialize the bfd library
+  bfd_init();
+
+  int total = 0;
+  unsigned int idx = numAddr;
+  for ( int i = 0; i < numAddr; i++ )
+  {
+     // find which executable, or library the symbol is from
+     FileMatch match( addrList[--idx] );
+     dl_iterate_phdr( findMatchingFile, &match );
+
+     // adjust the address in the global space of your binary to an
+     // offset in the relevant library
+     bfd_vma addr  = (bfd_vma)( addrList[idx] );
+             addr -= (bfd_vma)( match.mBase );
+
+     // lookup the symbol
+     if ( match.mFile && strlen( match.mFile ))
+        locations[idx] = processFile( match.mFile,      &addr, 1 );
+     else
+        locations[idx] = processFile( "/proc/self/exe", &addr, 1 );
+
+     total += strlen( locations[idx][0] ) + 1;
+  }
+
+  // return all the file and line information for each address
+  char** final = (char**)malloc( total + ( numAddr * sizeof( char* )));
+  char* f_strings = (char*)( final + numAddr );
+
+  for ( int i = 0; i < numAddr; i++ )
+  {
+     strcpy( f_strings, locations[i][0] );
+     free( locations[i] );
+     final[i] = f_strings;
+     f_strings += strlen( f_strings ) + 1;
+  }
+
+  return final;
+}*/
 
 #ifdef __cplusplus
 }
--- c/stack_dump_on_assert.h
+++ c/stack_dump_on_assert.h
@@ -1,7 +1,8 @@
 // Makes assert() print out a stack trace on unixlike systems. There is a windows version, but I did not implement.
 //To use, call setup_stack_dump_on_assert() from main.
+//Make sure to enable -rdynamic and -g to keep the symbols
 
-// Adapted from rafael on https://oroboro.com/stack-trace-on-crash/, who has graciously put it in the public domain
+// Adapted from rafael on https://oroboro.com/stack-trace-on-crash/, who has graciously put it in the public domain, and I also release my changes to the public domain
 #ifndef P_STACK_DUMP_ON_ASSERT_H
 #define P_STACK_DUMP_ON_ASSERT_H
 #ifdef __cplusplus
